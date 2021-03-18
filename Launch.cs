a6ff13008aaf67bda88b428463db8b2cde053b66
Jacob Johnson <jj.89@protonmail.com>
2021-03-18 00:04:12 -0500

    Added Launch class

---
+++ Launch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumeSpaceXApi
+{
+    class Launch
+    {
+        public int FlightNumber { get; set; }
+        public string MissionName { get; set; }
+        public bool Upcomming { get; set; }
+        public DateTime LaunchDateUtc { get; set; }
+    }
+}
